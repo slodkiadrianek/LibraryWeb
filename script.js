4cb5556a3058e6c52f40e16f6ce3b603ca3f818a
slodkiadrianek <adikurek11@gmail.com>
2024-02-04 15:44:12 +0100

    Signed-off-by: slodkiadrianek <adikurek11@gmail.com>

--- script.js
+++ script.js
@@ -2,19 +2,54 @@
 
 const quotePlace = document.querySelector(".text__quote");
 const quotePlaceAuthor = document.querySelector(".text__quote__author");
+const dailyBookImage = document.querySelector(".daily__book__image");
+const proposalBookText = document.querySelector(".proposal__book__text");
+const proposalAuthorText = document.querySelector(".proposal__author__text");
+
+const epikBook = [];
+const lirykBook = [];
+const dramaBook = [];
+// const
 
 class App {
   constructor() {
+    this.dataQuote;
+    this.dataBook;
+
     this.quoteApi();
+    this.bookAPI();
   }
   async quoteApi() {
     const response = await fetch("https://api.quotable.io/random");
-    const data = await response.json();
-    console.log(data);
-    const { content } = data;
-    const { author } = data;
+    this.dataQuote = await response.json();
+    const { content } = this.dataQuote;
+    const { author } = this.dataQuote;
     quotePlace.textContent = content;
     quotePlaceAuthor.textContent = author;
+  }
+  async bookAPI() {
+    const response = await fetch("https://wolnelektury.pl/api/books/");
+    this.dataBook = await response.json();
+    this.dataBook.map((el) => {
+      if (el.kind == "Epika") {
+        if (epikBook.length < 30) epikBook.push(el);
+      }
+      if (el.kind == "Liryka") {
+        if (lirykBook.length < 30) lirykBook.push(el);
+      }
+      if (el.kind == "Dramat") {
+        if (dramaBook.length < 30) dramaBook.push(el);
+      }
+    });
+    this._bookOfTheDay();
+  }
+  _bookOfTheDay() {
+    const allBooks = epikBook.concat(lirykBook).concat(dramaBook);
+    const indexOfBook = Math.trunc(Math.random() * 91);
+    const randomBook = allBooks[indexOfBook];
+    dailyBookImage.src = `${randomBook.simple_thumb}`;
+    proposalBookText.textContent = randomBook.title;
+    proposalAuthorText.textContent = randomBook.author;
   }
 }
 const app = new App();
