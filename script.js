5f0ba61f6ae407e6a41b30084f9c09045bbbf323
slodkiadrianek <adikurek11@gmail.com>
2024-02-04 13:26:53 +0100

    Signed-off-by: slodkiadrianek <adikurek11@gmail.com>

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 "use strict";
-const myObj = {};
-const h = document.getElementById("y");
-h.addEventListener("click", function (e) {
-  myObj.u = document.querySelector("#x").value;
 
-  let myObj_serialized = JSON.stringify(myObj);
+const quotePlace = document.querySelector(".text__quote");
+const quotePlaceAuthor = document.querySelector(".text__quote__author");
 
-  localStorage.setItem("myObj", myObj_serialized);
-});
+class App {
+  constructor() {
+    this.quoteApi();
+  }
+  async quoteApi() {
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    console.log(data);
+    const { content } = data;
+    const { author } = data;
+    quotePlace.textContent = content;
+    quotePlaceAuthor.textContent = author;
+  }
+}
+const app = new App();
 
-console.log(myObj.u);
-console.log(localStorage);
+// const myObj = {};
+// const h = document.getElementById("y");
+// h.addEventListener("click", function (e) {
+//   myObj.u = document.querySelector("#x").value;
+
+//   let myObj_serialized = JSON.stringify(myObj);
+
+//   localStorage.setItem("myObj", myObj_serialized);
+// });
+
+// console.log(myObj.u);
+// console.log(localStorage);
