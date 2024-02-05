71a563befec453af7033c63f110e85942f194c5a
slodkiadrianek <adikurek11@gmail.com>
2024-02-05 19:35:40 +0100

    Signed-off-by: slodkiadrianek <adikurek11@gmail.com>

--- animations.js
+++ animations.js
@@ -2,13 +2,12 @@
 
 const header = document.querySelector("#header");
 const section1 = document.querySelector("#section___1");
-
+const dailyBookImageBlured = document.querySelectorAll(".daily__book__image");
 const section1Heaight = section1.getBoundingClientRect().height;
-console.log(section1Heaight);
+const section = document.querySelectorAll(".section");
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) {
     header.classList.add("stickyheader");
   } else {
@@ -19,7 +18,26 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: `-${section1Heaight}px`,
+  rootMargin: `-${section1Heaight + 50}px`,
 });
 
 headerObserver.observe(section1);
+
+const blurEffect = function (entries, observer) {
+  const [imgEntry] = entries;
+  console.log(imgEntry);
+  if (!imgEntry.isIntersecting) return;
+  imgEntry.target.classList.remove("daily__book__image__blured");
+  observer.unobserve(imgEntry.target);
+};
+
+const imageObserver = new IntersectionObserver(blurEffect, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-300px",
+});
+
+section.forEach(function (section) {
+  //wybieranie każdej pokolei sekcji
+  imageObserver.observe(section);
+});
