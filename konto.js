cab9885e936172a61f225f0f5f449cf64d74c6b7
slodkiadrianek <adikurek11@gmail.com>
2024-02-14 11:51:43 +0100

    Signed-off-by: slodkiadrianek <adikurek11@gmail.com>

--- konto.js
+++ konto.js
@@ -4,9 +4,11 @@ const showTime = document.querySelector(".show__time");
 const inputFile = document.querySelector(".input__file");
 const profileImg = document.querySelector(".profile__img");
 const showEmail = document.querySelector(".showEmail");
-const listedBookReserved = document.querySelector(".listed__book__reserved");
 const listedBookLiked = document.querySelector(".listed__book__liked");
-
+const liked = document.querySelector(".liked");
+const reserved = document.querySelector(".reserved");
+const changeH = document.querySelector(".changeH");
+const changeL = document.querySelector(".changeL");
 const data = JSON.parse(sessionStorage.getItem("user"));
 
 const x = data.liked;
@@ -19,7 +21,11 @@ class App {
     setInterval(() => this._showTime(), 1000);
     inputFile.addEventListener("change", this._showProfileImage);
     this._showEmail();
-    this._listBooks();
+    this._likeBooks();
+    liked.addEventListener("click", this._likeBooks);
+    reserved.addEventListener("click", this._reservedBooks);
+    changeL.addEventListener("click", this._showChangeLogin.bind(this));
+    changeH.addEventListener("click", this._showChangePass.bind(this));
   }
   _showUserName() {
     helloUser.textContent = `Witaj ${data.login}`;
@@ -33,33 +39,88 @@ class App {
   }
   _showProfileImage() {
     profileImg.src = URL.createObjectURL(inputFile.files[0]);
-    data.profileImg = profileImg.src.split("blob:http://127.0.0.1:5500/")[1];
-
-    sessionStorage.setItem("user", JSON.stringify(data));
   }
   _showEmail() {
     showEmail.textContent = `Twój email to ${data.email}`;
   }
-  _listBooks() {
-    console.log(data.liked);
-    console.log(data.reserved);
-    data.reserved.forEach((el) => {
-      const likedBooks = `
-    <div>
-              <p>${el}</p>
-              </div>
-    `;
-      listedBookReserved.insertAdjacentHTML("afterbegin", likedBooks);
-    });
-
+  _likeBooks() {
+    listedBookLiked.textContent = "";
     data.likes.forEach((el) => {
       const likedBooks = `
-    <div>
-              <p>${el}</p>
-              </div>
+      <p class="book_liked">${el}</p>
     `;
       listedBookLiked.insertAdjacentHTML("afterbegin", likedBooks);
     });
+  }
+  _reservedBooks() {
+    listedBookLiked.textContent = "";
+    data.reserve.forEach((el) => {
+      const likedBooks = `
+      <p class="book_liked">${el}</p>
+    `;
+      listedBookLiked.insertAdjacentHTML("afterbegin", likedBooks);
+    });
+  }
+  _showChangeLogin() {
+    listedBookLiked.textContent = "";
+    const likedBooks = `
+      <article class="change__login">
+              <input
+                class="input__change newLogin"
+                type="text"
+                placeholder="Wprowadź nowy login"
+                required
+              />
+              <button class="input__change acceptChanges">Zatwierdź</button>
+            </article>
+    `;
+    listedBookLiked.insertAdjacentHTML("afterbegin", likedBooks);
+    const newLogin = document.querySelector(".newLogin");
+    const acceptChanges = document.querySelector(".acceptChanges");
+    acceptChanges.addEventListener(
+      "click",
+      function () {
+        data.login = newLogin.value;
+        this._showUserName();
+        newLogin.value = "";
+      }.bind(this)
+    );
+  }
+
+  _showChangePass() {
+    listedBookLiked.textContent = "";
+    const likedBooks = `
+      <article class="change__login">
+          <input
+                class="input__change oldPass"
+                type="text"
+                placeholder="Wprowadź stare hasło"
+                required
+              />
+              <input
+                class="input__change newPass"
+                type="text"
+                placeholder="Wprowadź nowe hasło"
+                required
+              />
+              <button class="input__change acceptChanges">Zatwierdź</button>
+            </article>
+    `;
+    listedBookLiked.insertAdjacentHTML("afterbegin", likedBooks);
+    const oldPass = document.querySelector(".oldPass");
+    const newPass = document.querySelector(".newPass");
+    const acceptChanges = document.querySelector(".acceptChanges");
+    acceptChanges.addEventListener(
+      "click",
+      function () {
+        if (oldPass.value === data.password) {
+          data.password = newPass.value;
+          oldPass.value = newPass.value = "";
+        } else {
+          alert("wprowadź poprawne");
+        }
+      }.bind(this)
+    );
   }
 }
 
